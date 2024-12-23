ead218211b6d327522af920827aafe216b492aa4
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-12-23 23:44:02 +0100

    Add Counter component with time-themed functionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+
 function Counter() {
-    return (
-      <div>
-        <h1>Contador del Tiempo</h1>
-        <p>Aquí irá la funcionalidad del contador.</p>
+  // Estado inicial para el contador
+  const [count, setCount] = useState(0);
+
+  // Funciones para manejar el estado del contador
+
+  const handleIncrement = () => setCount(count + 1);
+
+  const handleDecrement = () => setCount(count - 1);
+
+  const handleReset = () => setCount(0);
+
+  return (
+    <div className="counter__container">
+      <h1>Contador del Tiempo</h1>
+      <h2 className="counter__number">{count}</h2>
+      <div className="counter__buttons">
+        <button className="counter__button" onClick={handleIncrement}>
+          Avanzar
+        </button>
+        <button className="counter__button" onClick={handleDecrement}>
+          Retroceder
+        </button>
+        <button className="counter__button" onClick={handleReset}>
+          Volver al presente
+        </button>
       </div>
-    );
-  }
-  
-  export default Counter;
+    </div>
+  );
+}
+
+export default Counter;
